0d85317946c851945fcee9dfdcb460873ff432da
gauravk403@gmail.com <gauravk403@gmail.com>
2021-01-27 22:30:44 +0530

    Updated app by ading shop page, collection-preview and collection-item component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter> 
     <App />
     </BrowserRouter>,
   document.getElementById('root')
