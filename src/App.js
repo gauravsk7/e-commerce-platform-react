0f4e145a4921da7e098ee4193b25d958582a4c3d
gauravk403@gmail.com <gauravk403@gmail.com>
2021-03-03 18:11:01 +0530

    Added sign up feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 import './App.css';
 import { Switch, Route } from 'react-router-dom'; 
 
@@ -9,7 +8,7 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component{
 
@@ -27,9 +26,27 @@ class App extends React.Component{
 
 
   componentDidMount(){             //This is an open subscription, we need to close it when app is unmounted
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user})
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = createUserProfileDocument(userAuth) 
+
+        ;(await userRef).onSnapshot(snapShot => {
+          this.setState(
+            {
+            currentUser: {
+            id: snapShot.id,  
+            ...snapShot.data()}
+            }
+            ,() => {
+             console.log(this.state)    //setState is asynchronous, so when logging it in the console include that in a callback
+          }
+          )
+        })
+      }
+      else{
+        this.setState({currentUser: userAuth})
+      }
+
     })
   }
 
