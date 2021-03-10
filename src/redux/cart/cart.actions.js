6f90128459dca6cb3eeef6ab331a4f1e173ebc00
gauravk403@gmail.com <gauravk403@gmail.com>
2021-03-10 13:05:56 +0530

    Updated shop data, grouped cart items together

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CartActionTypes from './cart.types'
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN   //payload not needed here, it is optional
 })
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
