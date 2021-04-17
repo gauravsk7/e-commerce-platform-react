520b444fe23ab511cc732d5188f65fe08073d82d
gauravk403@gmail.com <gauravk403@gmail.com>
2021-04-17 13:05:48 +0530

    moved shop and directory data into its respective reducers, added selectors for the same and added collection pages

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import { persistStore } from 'redux-persist'
 
 import rootReducer from './root-reducer'
 
 const middlewares = [logger]   //This makes the app more scalable, we can add as many middlewares as needed later
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))   //the ... syntax here ensures that the array elements are passed to the function as independent arguments
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))   //the ... syntax here ensures that the array elements are passed to the function as independent arguments
 
-export default store
+export const persistor = persistStore(store)
+
+export default { store, persistor }
